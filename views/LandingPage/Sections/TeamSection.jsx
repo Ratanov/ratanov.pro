4aac94056e81aebb9bec8223026f9b6270d5fa73
Oleg Ratanov <displacer88@gmail.com>
2019-05-19 04:03:44 +0400

    fix Поправил нашу команду на главной

--- views/LandingPage/Sections/TeamSection.jsx
+++ views/LandingPage/Sections/TeamSection.jsx
@@ -37,6 +37,47 @@ class TeamSection extends React.Component {
           <GridContainer>
             <GridItem xs={12} sm={12} md={4}>
               <Card plain>
+
+                <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
+                  <Link to={"/olegratanov"} className={classes.link}>
+                    <img src={ratanov} alt="..." className={imageClasses} />
+                  </Link>
+                </GridItem>
+                <h4 className={classes.cardTitle}>
+                  <Link to={"/olegratanov"} className={classes.link}>
+                    Олег Ратанов
+                  </Link>
+                  <br />
+                  <small className={classes.smallTitle}>Designer</small>
+                </h4>
+                <CardBody>
+                  <p className={classes.description}>
+                    You can write here details about one of your team members.
+                    You can give more details about what they do. Feel free to
+                    add some <a href="#pablo">links</a> for people to be able to
+                    follow them outside the site.
+                  </p>
+                </CardBody>
+                <CardFooter className={classes.justifyCenter}>
+                  <Button
+                    justIcon
+                    color="transparent"
+                    className={classes.margin5}
+                  >
+                    <i className={classes.socials + " fab fa-twitter"} />
+                  </Button>
+                  <Button
+                    justIcon
+                    color="transparent"
+                    className={classes.margin5}
+                  >
+                    <i className={classes.socials + " fab fa-linkedin"} />
+                  </Button>
+                </CardFooter>
+              </Card>
+            </GridItem>
+            <GridItem xs={12} sm={12} md={4}>
+              <Card plain>
                 <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
                   <img src={team1} alt="..." className={imageClasses} />
                 </GridItem>
@@ -74,47 +115,6 @@ class TeamSection extends React.Component {
                     className={classes.margin5}
                   >
                     <i className={classes.socials + " fab fa-facebook"} />
-                  </Button>
-                </CardFooter>
-              </Card>
-            </GridItem>
-            <GridItem xs={12} sm={12} md={4}>
-              <Card plain>
-
-                <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
-                  <Link to={"/olegratanov"} className={classes.link}>
-                    <img src={ratanov} alt="..." className={imageClasses} />
-                  </Link>
-                </GridItem>
-                <h4 className={classes.cardTitle}>
-                  <Link to={"/olegratanov"} className={classes.link}>
-                    Олег Ратанов
-                  </Link>
-                  <br />
-                  <small className={classes.smallTitle}>Designer</small>
-                </h4>
-                <CardBody>
-                  <p className={classes.description}>
-                    You can write here details about one of your team members.
-                    You can give more details about what they do. Feel free to
-                    add some <a href="#pablo">links</a> for people to be able to
-                    follow them outside the site.
-                  </p>
-                </CardBody>
-                <CardFooter className={classes.justifyCenter}>
-                  <Button
-                    justIcon
-                    color="transparent"
-                    className={classes.margin5}
-                  >
-                    <i className={classes.socials + " fab fa-twitter"} />
-                  </Button>
-                  <Button
-                    justIcon
-                    color="transparent"
-                    className={classes.margin5}
-                  >
-                    <i className={classes.socials + " fab fa-linkedin"} />
                   </Button>
                 </CardFooter>
               </Card>
